581df6417ac0d1452aaf1eac05160f100f003fe0
Jan-OleElderts <56040890+Jan-OleElderts@users.noreply.github.com>
2019-10-10 13:51:50 +0200

    Hinzufügen von Einheiten

--- profile_system/profile_system_v20191010/Program.cs
+++ profile_system/profile_system_v20191010/Program.cs
@@ -20,35 +20,35 @@ namespace profile_system_v20191010
             //Vorgeplänkel und Eingabeabfrage zum Recheckprofil
             Console.WriteLine("Wilkommen zu Ihrem persöhnlichem Profilgenerierungssystem.");
 
-            Console.WriteLine("Bitte geben Sie die Höhe des Rechteckprofils ein.");
+            Console.WriteLine("Bitte geben Sie die Höhe des Rechteckprofils im Metern ein und bestätigen Sie mit 'Enter'.");
             String StringRechteckH = Console.ReadLine();
             Double RechteckH = Convert.ToDouble(StringRechteckH);
 
-            Console.WriteLine("Bitte geben Sie die Breite des Rechteckprofils ein.");
-            String  StringRechteckB = Console.ReadLine();
+            Console.WriteLine("Bitte geben Sie die Breite des Rechteckprofils in Metern ein und bestätigen Sie mit 'Enter'.");
+            String StringRechteckB = Console.ReadLine();
             Double RechteckB = Convert.ToDouble(StringRechteckB);
 
-            Console.WriteLine("Bitte geben Sie die Tiefe des Rechteckprofils ein.");
+            Console.WriteLine("Bitte geben Sie die Tiefe des Rechteckprofils in Metern ein und bestätigen Sie mit 'Enter'.");
             String StringRechteckT = Console.ReadLine();
             Double RechteckT = Convert.ToDouble(StringRechteckT);
 
-            Console.WriteLine("Bitte geben Sie die Dichte des gewünschten Materials des Rechteckprofils ein.");
+            Console.WriteLine("Bitte geben Sie die Dichte des gewünschten Materials des Rechteckprofils in Kg/m^3 ein und bestätigen Sie mit 'Enter'.");
             String StringDichte = Console.ReadLine();
             Double Dichte = Convert.ToDouble(StringDichte);
 
             // Berechnungen aus Eingabe
             Double Querschnitt = RechteckH * RechteckB;
-            Double Volumen = RechteckH * RechteckB * RechteckT ;
+            Double Volumen = RechteckH * RechteckB * RechteckT;
             Double Gewicht = Volumen * Dichte;
-            Double Flaechentraegheitsmomenty = Querschnitt * (Math.Sqrt(RechteckH)/12);
-            Double Flaechentraegheitsmomentz = Querschnitt * (Math.Sqrt(RechteckB)/12);
+            Double Flaechentraegheitsmomenty = Querschnitt * (Math.Sqrt(RechteckH) / 12);
+            Double Flaechentraegheitsmomentz = Querschnitt * (Math.Sqrt(RechteckB) / 12);
 
             // Ausgabe der Berechneten Werte
-            Console.WriteLine("Querschnitt: " + Querschnitt);
-            Console.WriteLine("Volumen: " + Volumen);
-            Console.WriteLine("Gewicht: " + Gewicht);
-            Console.WriteLine("Flächenträgheitsmoment in Y-Richtung: " + Flaechentraegheitsmomenty);
-            Console.WriteLine("Flächenträgheitsmoment in Z-Richtung: " + Flaechentraegheitsmomentz);
+            Console.WriteLine("Querschnitt: " + Querschnitt + "m^2");
+            Console.WriteLine("Volumen: " + Volumen + "m^3");
+            Console.WriteLine("Gewicht: " + Gewicht + "Kg");
+            Console.WriteLine("Flächenträgheitsmoment in Y-Richtung: " + Flaechentraegheitsmomenty + "m^4");
+            Console.WriteLine("Flächenträgheitsmoment in Z-Richtung: " + Flaechentraegheitsmomentz + "m^4");
 
             Console.ReadKey();
         }

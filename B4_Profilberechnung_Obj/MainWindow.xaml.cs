b94f0914bc97291710ab2dbf4d3d55b21e36d5d5
Jan-OleElderts <56040890+Jan-OleElderts@users.noreply.github.com>
2019-11-07 06:56:00 +0100

    Hinzufügen von Grundstruckturen

--- B4_Profilberechnung_Obj/MainWindow.xaml.cs
+++ B4_Profilberechnung_Obj/MainWindow.xaml.cs
@@ -22,7 +22,78 @@ namespace B4_Profilberechnung_Obj
     {
         public MainWindow()
         {
+            
             InitializeComponent();
+            Hide();
         }
+
+        // private void ZeigeBild(string strImage)
+        //  {
+        //      BitmapImage btMap;
+        //     btMap.BeginInit();
+        //      btMap.UriSource = new Uri(strImage, UriKind.Relative);
+        //      btMap.EndInit();
+        //      imFigur.Visibility = Visibility.Visible;
+        //      imFigur.Source = btMap;
+        //   }
+
+
+        private void Button_Click_Ende(object sender, RoutedEventArgs e)
+        {
+            //schließt die Anwendung
+            Close();
+        }
+        private void Button_Click_Reset(object sender, RoutedEventArgs e)
+        {
+            //setzt die Parameter in den Textboxen zurück
+            txtParameter1.Text = "";
+            txtParameter2.Text = "";
+            txtParameter3.Text = "";
+            txtParameter4.Text = "";
+            txtParameter5.Text = "";
+            txtParameter6.Text = "";
+        }
+
+        private void Button_Click_Berechne(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            Hide();
+            ListBoxItem Rechteck = ((sender as ListBox).SelectedItem as ListBoxItem);
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            Hide();
+           
+                          
+
+        }
+
+        private new void Hide()
+        {
+            // versteckt alle Textfelder und Parameterboxen
+            imFigur.Visibility = Visibility.Hidden;
+            lblParameter1.Visibility = Visibility.Hidden;
+            lblParameter2.Visibility = Visibility.Hidden;
+            lblParameter3.Visibility = Visibility.Hidden;
+            lblParameter4.Visibility = Visibility.Hidden;
+            lblParameter5.Visibility = Visibility.Hidden;
+            lblParameter6.Visibility = Visibility.Hidden;
+
+            txtParameter1.Visibility = Visibility.Hidden;
+            txtParameter2.Visibility = Visibility.Hidden;
+            txtParameter3.Visibility = Visibility.Hidden;
+            txtParameter4.Visibility = Visibility.Hidden;
+            txtParameter5.Visibility = Visibility.Hidden;
+            txtParameter6.Visibility = Visibility.Hidden;
+            btnBerechne.IsEnabled=false;
+        }
+
+               
     }
 }

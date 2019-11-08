f4ea96e18dcf2ee291faf937d149c6dbdf83cafe
Jan-OleElderts <56040890+Jan-OleElderts@users.noreply.github.com>
2019-11-08 23:15:58 +0100

    Implementieren von allen Profilen
    Es wurde die Auswahl von allen Profilen und die korrekte Anzeige von allen Parametern hinzugefügt.

--- B4_Profilberechnung_Obj/MainWindow.xaml.cs
+++ B4_Profilberechnung_Obj/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MessageBox = System.Windows.MessageBox;
 
 namespace B4_Profilberechnung_Obj
 {
@@ -24,23 +25,19 @@ namespace B4_Profilberechnung_Obj
 
             InitializeComponent();
             Hide();
-        }
-
-        private void ZeigeBild(string strImage)
-        {
-            //      BitmapImage btMap;
-            //      btMap.BeginInit();
-            //      btMap.UriSource = new Uri(strImage, UriKind.Relative);
-            //      btMap.EndInit();
-            //      imFigur.Visibility = Visibility.Visible;
-            //      imFigur.Source = btMap;
-        }
-
-
+        }             
+        
         private void Button_Click_Ende(object sender, RoutedEventArgs e)
         {
             //schließt die Anwendung
-            Close();
+            MessageBoxResult msgAbfrage = MessageBox.Show("Wirklich Beenden?", "Wirklich Beenden?",
+                                                          MessageBoxButton.YesNo);
+           
+            if (msgAbfrage == MessageBoxResult.Yes)
+            { 
+                Close(); 
+            }
+            
         }
         private void Button_Click_Reset(object sender, RoutedEventArgs e)
         {
@@ -60,14 +57,21 @@ namespace B4_Profilberechnung_Obj
 
         private new void Hide()
         {
-            // versteckt alle Textfelder und Parameterboxen
-            imFigur.Visibility = Visibility.Hidden;
+            // versteckt alle Textfelder und Parameterboxen und Bilder
+            imRechteck.Visibility = Visibility.Hidden;
+            imKasten.Visibility = Visibility.Hidden;
+            imDreieck.Visibility = Visibility.Hidden;
+            imKreisring.Visibility = Visibility.Hidden;
+            imUProfil.Visibility = Visibility.Hidden;
+            imIProfil.Visibility = Visibility.Hidden;
+
             lblParameter1.Visibility = Visibility.Hidden;
             lblParameter2.Visibility = Visibility.Hidden;
             lblParameter3.Visibility = Visibility.Hidden;
             lblParameter4.Visibility = Visibility.Hidden;
             lblParameter5.Visibility = Visibility.Hidden;
             lblParameter6.Visibility = Visibility.Hidden;
+            lblHeader.Visibility = Visibility.Hidden;
 
             txtParameter1.Visibility = Visibility.Hidden;
             txtParameter2.Visibility = Visibility.Hidden;
@@ -78,52 +82,145 @@ namespace B4_Profilberechnung_Obj
             btnBerechne.IsEnabled = false;
 
         }
-
+        
         public void Rechteck()
         {
+            // die für den ausgewählten Fall benötigten Label und Textboxen, sowie das passende Bild werden hier eingebledet
             lblParameter1.Visibility = Visibility.Visible;
             lblParameter2.Visibility = Visibility.Visible;
             lblParameter3.Visibility = Visibility.Visible;
+            lblHeader.Visibility = Visibility.Visible;
             txtParameter1.Visibility = Visibility.Visible;
             txtParameter2.Visibility = Visibility.Visible;
             txtParameter3.Visibility = Visibility.Visible;
+            imRechteck.Visibility = Visibility.Visible;
 
-            lblParameter1.Content = "Höhe:";
-            lblParameter2.Content = "Breite:";
+            // Beschriftung der Labels
+            lblParameter1.Content = "h:";
+            lblParameter2.Content = "b:";
             lblParameter3.Content = "Tiefe:";
+            lblHeader.Content = "Rechteck";
 
             btnBerechne.IsEnabled = true;
         }
         public void Dreieck()
         {
+            // die für den ausgewählten Fall benötigten Label und Textboxen, sowie das passende Bild werden hier eingebledet
             lblParameter1.Visibility = Visibility.Visible;
             lblParameter2.Visibility = Visibility.Visible;
             lblParameter3.Visibility = Visibility.Visible;
+            lblHeader.Visibility = Visibility.Visible;
             txtParameter1.Visibility = Visibility.Visible;
             txtParameter2.Visibility = Visibility.Visible;
             txtParameter3.Visibility = Visibility.Visible;
+            imDreieck.Visibility = Visibility.Visible;
 
-            lblParameter1.Content = "Höhe:";
-            lblParameter2.Content = "Breite:";
+            // Beschriftung der Labels
+            lblParameter1.Content = "h:";
+            lblParameter2.Content = "a:";
             lblParameter3.Content = "Tiefe:";
+            lblHeader.Content = "Dreieck";
 
             btnBerechne.IsEnabled = true;
         }
         public void Kasten()
         {
+            // die für den ausgewählten Fall benötigten Label und Textboxen, sowie das passende Bild werden hier eingebledet
+            lblParameter1.Visibility = Visibility.Visible;
+            lblParameter2.Visibility = Visibility.Visible;
+            lblParameter3.Visibility = Visibility.Visible;
+            lblParameter4.Visibility = Visibility.Visible;
+            lblParameter5.Visibility = Visibility.Visible;
+            lblHeader.Visibility = Visibility.Visible;
+            txtParameter1.Visibility = Visibility.Visible;
+            txtParameter2.Visibility = Visibility.Visible;
+            txtParameter3.Visibility = Visibility.Visible;
+            txtParameter4.Visibility = Visibility.Visible;
+            txtParameter5.Visibility = Visibility.Visible;
+            imKasten.Visibility = Visibility.Visible;
 
+            // Beschriftung der Labels
+            lblParameter1.Content = "H:";
+            lblParameter2.Content = "h:";
+            lblParameter3.Content = "B:";
+            lblParameter4.Content = "b:";
+            lblParameter5.Content = "Tiefe:";
+            lblHeader.Content = "Kasten";
+
+            btnBerechne.IsEnabled = true;
+            
         }
-        public void Kreis()
+        public void Kreisring()
         {
+            // die für den ausgewählten Fall benötigten Label und Textboxen, sowie das passende Bild werden hier eingebledet
+            lblParameter1.Visibility = Visibility.Visible;
+            lblParameter2.Visibility = Visibility.Visible;
+            lblParameter3.Visibility = Visibility.Visible;
+            lblHeader.Visibility = Visibility.Visible;
+            txtParameter1.Visibility = Visibility.Visible;
+            txtParameter2.Visibility = Visibility.Visible;
+            txtParameter3.Visibility = Visibility.Visible;
+            imKreisring.Visibility = Visibility.Visible;
 
+            // Beschriftung der Labels
+            lblParameter1.Content = "R:";
+            lblParameter2.Content = "r:";
+            lblParameter3.Content = "Tiefe:";
+            lblHeader.Content = "Kreisring";
+
+            btnBerechne.IsEnabled = true;
         }
         public void UProfil()
         {
+            // die für den ausgewählten Fall benötigten Label und Textboxen, sowie das passende Bild werden hier eingebledet
+            lblParameter1.Visibility = Visibility.Visible;
+            lblParameter2.Visibility = Visibility.Visible;
+            lblParameter3.Visibility = Visibility.Visible;
+            lblParameter4.Visibility = Visibility.Visible;
+            lblParameter5.Visibility = Visibility.Visible;
+            lblHeader.Visibility = Visibility.Visible;
+            txtParameter1.Visibility = Visibility.Visible;
+            txtParameter2.Visibility = Visibility.Visible;
+            txtParameter3.Visibility = Visibility.Visible;
+            txtParameter4.Visibility = Visibility.Visible;
+            txtParameter5.Visibility = Visibility.Visible;
+            imUProfil.Visibility = Visibility.Visible;
 
+            // Beschriftung der Labels
+            lblParameter1.Content = "H:";
+            lblParameter2.Content = "h:";
+            lblParameter3.Content = "B:";
+            lblParameter4.Content = "b:";
+            lblParameter5.Content = "Tiefe:";
+            lblHeader.Content = "U-Profil";
+
+            btnBerechne.IsEnabled = true;
         }
         public void IProfil()
         {
+            // die für den ausgewählten Fall benötigten Label und Textboxen, sowie das passende Bild werden hier eingebledet
+            lblParameter1.Visibility = Visibility.Visible;
+            lblParameter2.Visibility = Visibility.Visible;
+            lblParameter3.Visibility = Visibility.Visible;
+            lblParameter4.Visibility = Visibility.Visible;
+            lblParameter5.Visibility = Visibility.Visible;
+            lblHeader.Visibility = Visibility.Visible;
+            txtParameter1.Visibility = Visibility.Visible;
+            txtParameter2.Visibility = Visibility.Visible;
+            txtParameter3.Visibility = Visibility.Visible;
+            txtParameter4.Visibility = Visibility.Visible;
+            txtParameter5.Visibility = Visibility.Visible;
+            imIProfil.Visibility = Visibility.Visible;
 
+            // Beschriftung der Labels
+            lblParameter1.Content = "H:";
+            lblParameter2.Content = "h:";
+            lblParameter3.Content = "B:";
+            lblParameter4.Content = "b/2:";
+            lblParameter5.Content = "Tiefe:";
+            lblHeader.Content = "I-Profil";
+
+            btnBerechne.IsEnabled = true;
         }
 
 
@@ -133,16 +230,16 @@ namespace B4_Profilberechnung_Obj
 
             // Test um mit dem Debugger zu testen, was denn überhaupt für ein Typ übergeben wir
             // Mit einem "object" kann man nicht wirklich arbeiten - var kann alles aufnehmen was kommt
-            var eventargs = e.NewValue;
-            var send = sender;
+            //    var eventargs = e.NewValue;
+            //    var send = sender;
 
             // Variante 2
-            TreeViewItem myE = (TreeViewItem) e.NewValue  ;
-            string Variable = myE.Name;
+            //  TreeViewItem myE = (TreeViewItem) e.NewValue  ;
+            //   string Variable = myE.Name;
 
             // Variante 3
-            TreeViewItem auswahlItem = (TreeViewItem) trvFigur.SelectedItem;
-            Variable = auswahlItem.Name;
+            TreeViewItem auswahlItem = (TreeViewItem)trvFigur.SelectedItem;
+            string Variable = auswahlItem.Name;
 
             switch (Variable)
             {
@@ -155,8 +252,8 @@ namespace B4_Profilberechnung_Obj
                 case ("itmKasten"):
                     Kasten();
                     break;
-                case ("itmKreis"):
-                    Kreis();
+                case ("itmKreisring"):
+                    Kreisring();
                     break;
                 case ("itmUProfil"):
                     UProfil();
@@ -164,25 +261,23 @@ namespace B4_Profilberechnung_Obj
                 case ("itmIProfil"):
                     IProfil();
                     break;
-
-
             }
-
         }
+    }
 
-        private void itmRechteck_Selected(object sender, RoutedEventArgs e)
-        {
-            // Variante 1
-            var ev = e.Source;
-            string auswahl1 = ((TreeViewItem)e.Source).Name.ToString();
+       // private void itmRechteck_Selected(object sender, RoutedEventArgs e)
+       //  {
+            //  Variante 1
+            //  var ev = e.Source;
+            //  string auswahl1 = ((TreeViewItem)e.Source).Name.ToString();
 
             // Variante 4 
-            var send = sender;
-            string auswahl2 = ((TreeViewItem)sender).Name.ToString();
+            //   var send = sender;
+            //   string auswahl2 = ((TreeViewItem)sender).Name.ToString();
 
             // hier brauchen Sie die switch/case Abfrage nicht, denn sie wissen schon, dass itmRechteck die Quelle ist
             // Danke für die Fingerübung!
-        }
+       //  }
 
     }    
-}
+
